0237a015e9d32936073bad912b92f398ebc0c4a3
uditrajsingh <uditr.singh@decisionpoint.in>
2025-03-24 15:57:51 +0530

    updated

--- chatbot2.py
+++ chatbot2.py
@@ -4,7 +4,9 @@ import bcrypt
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain.vectorstores import FAISS
+# from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from PyPDF2 import PdfReader
